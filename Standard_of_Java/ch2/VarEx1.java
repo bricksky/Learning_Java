a4f099ba97b95a131133fce18f79a223bd121fd0
bricksky <skydh1214@naver.com>
2023-02-14 17:54:30 +0900

    변수의 선언과 초기화

--- Standard_of_Java/ch2/VarEx1.java
+++ Standard_of_Java/ch2/VarEx1.java
@@ -6,10 +6,15 @@ class VarEx1 {
         System.out.println(year);
         System.out.println(age);
 
-        year = age + 2000;
-        age = age + 1;
+        year = age + 2000; // 변수 age의 값에 2000을 더해 변수 year에 저장한다.
+        age = age + 1; // 변수 age에 저장된 값에 1을 더한다.
 
         System.out.println(year);
         System.out.println(age);
     }
 }
+
+/*
+ * 1. 글자를 출력하려면 println을 사용한다.
+ * 2. 변수에 저장된 값을 사용하려면 변수의 이름만 적으면 된다.
+ */
