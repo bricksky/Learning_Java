a14f35ebdb65884c7f3d143b73d4a2ca0cdb1753
bricksky <skydh1214@naver.com>
2023-02-19 22:25:52 +0900

    형변환 방법3

---
+++ Standard_of_Java/ch2/CastingEx3.java
@@ -0,0 +1,11 @@
+public class CastingEx3 {
+    public static void main(String[] args) {
+        float f = 9.1234567f;
+        double d = 9.1234567;
+        double d2 = (double) f;
+
+        System.out.printf("f=%20.18f\n", f);
+        System.out.printf("d=%20.18f\n", d); // 전체 20자리 중에서 18자리는 소수점 이하의 수를 출력하세요!! (교재 70쪽 참고)
+        System.out.printf("d2=%20.18f\n", d2);
+    }
+}
