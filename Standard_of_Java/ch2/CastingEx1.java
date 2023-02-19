49eeae5d111461676984305ba0d89dfe4347dfd1
bricksky <skydh1214@naver.com>
2023-02-19 22:02:06 +0900

    형 변환 방법1

---
+++ Standard_of_Java/ch2/CastingEx1.java
@@ -0,0 +1,9 @@
+public class CastingEx1 {
+    public static void main(String[] args) {
+        double d = 85.4;
+        int score = (int) d; // double형을 int형으로 바꾼다는 의미임
+
+        System.out.println("score= " + score);
+        System.out.println("d=" + d);
+    }
+}
