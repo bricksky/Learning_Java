64e3998b682746267a766220129613c847405d1e
bricksky <skydh1214@naver.com>
2023-02-20 16:32:14 +0900

    증감연산자 ++ --

---
+++ Standard_of_Java/ch3/OperatorEx1.java
@@ -0,0 +1,15 @@
+package ch3;
+
+public class OperatorEx1 {
+    public static void main(String args[]) {
+        int i = 5;
+        i++; // i=i+1과 같은 의미이다.
+        System.out.println(i);
+        i = 5;
+        ++i;
+        System.out.println(i);
+    }
+
+    // 어떤 수식에 포함되거나 메서드의 매개변수로 사용된 경우에는 다른 값이 나온다.
+
+}
