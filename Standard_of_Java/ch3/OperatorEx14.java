52137e7277a22b6cf0a8f8b113ea8aa054d2239d
bricksky <skydh1214@naver.com>
2023-02-23 23:05:02 +0900

    사칙 연산자 형 변환

---
+++ Standard_of_Java/ch3/OperatorEx14.java
@@ -0,0 +1,22 @@
+package ch3;
+
+public class OperatorEx14 {
+    public static void main(String[] args) {
+        char c = 'a';
+        for (int i = 0; i < 26; i++) {
+            System.out.print(c++); // a부터 26개의 문자를 출력
+        }
+        System.out.println();
+
+        c = 'A';
+        for (int i = 0; i < 26; i++) {
+            System.out.print(c++); // A부터 26개의 문자를 출력
+        }
+        System.out.println();
+        c = '0';
+        for (int i = 0; i < 10; i++) {
+            System.out.print(c++);// 1부터 10개의 문자를 출력
+        }
+        System.out.println();
+    }
+}
