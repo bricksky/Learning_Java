aa2a93d75c1b2e33b7fa68de228dd6b5e979de3b
bricksky <skydh1214@naver.com>
2023-03-03 14:31:23 +0900

    조건문 switch문의 제약조건

--- Standard_of_Java/ch4/FlowEx10.java
+++ Standard_of_Java/ch4/FlowEx10.java
@@ -30,3 +30,13 @@ public class FlowEx10 {
         System.out.println("당신의 학점은" + grade + "입니다.");
     }
 }
+
+/*
+ * int형 나누기 int형은 int이다.
+ * 함수를 선언할 때, score를 int형으로 바꾸었고, switch문에서 int형으로 나누기에 값도 int형으로 나온다.
+ * 88 나누기 10은 정수형으로 나오기에 8.8이 아닌 8로 값이 출력된다.
+ * 책에서도 나왔지만, switch부분에서 간략하게 식을 작성해 코드를 짧게 쓰는 것이 좋다고 한다.
+ * 
+ * 실수
+ * -switch 문 이후 break를 작성하지 않았다.
+ */
