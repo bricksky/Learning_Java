1d3792955a942e77fc7ba9703a08d1447d0518ac
bricksky <skydh1214@naver.com>
2024-07-06 22:46:04 +0900

    변수 타입2

---
+++ Introduction_Java/Java-start/src/variable/Var8.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var8 {
+
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127;   // -128~127까지
+        short s = 32767; // -32.768 ~ 32,767까지
+        int i = 214748367;   //-2,147,483,648 ~ 2,147,483,647
+
+        // 실수
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
+
+// 정수는 기본형으로 int를, 실수는 기본형으로 double을 사용한다.
