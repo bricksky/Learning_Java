40894b753a18545072b697cdad005a0dcc266a9e
bricksky <skydh1214@naver.com>
2024-07-06 23:30:55 +0900

    문제와 풀이

---
+++ Introduction_Java/Java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long answer1 = 10000000000L;
+        System.out.println(answer1);
+
+        boolean answer2 = true;
+        System.out.println(answer2);
+    }
+}
