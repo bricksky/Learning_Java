8b69d1d475489c718cd48c1eacdc03ec38e91170
bricksky <skydh1214@naver.com>
2024-07-06 22:23:11 +0900

    변수 타입1

---
+++ Introduction_Java/Java-start/src/variable/Var7.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100;    // 정수를 담을 수 있음
+        double b = 10.5;    // 실수를 담을 수 있음
+        boolean c = true;   // true와 false만 담을 수 있음
+        char d = 'A';   // 문자 하나를 담을 수 있음
+        String e = "Hello Java";    //문자열을 담을 수 있음
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
