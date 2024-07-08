5ca8a5bca10d16b1a421cf93ee61b126fd8c1aa1
bricksky <skydh1214@naver.com>
2024-07-08 23:07:03 +0900

    while문2

--- Introduction_Java/Java-start/src/loop/While1.java
+++ Introduction_Java/Java-start/src/loop/While1.java
@@ -1,6 +1,6 @@
 package loop;
 
-public class While_1 {
+public class While1 {
 
     public static void main(String[] args) {
         int count  = 0;
--- Introduction_Java/Java-start/src/loop/While2.java
+++ Introduction_Java/Java-start/src/loop/While2.java
@@ -1,6 +1,6 @@
 package loop;
 
-public class While_2 {
+public class While2 {
 
     public static void main(String[] args) {
         int count  = 0;
