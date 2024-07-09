0e52fda785ffa195a8ab865570b18c3bf292c53e
bricksky <skydh1214@naver.com>
2024-07-09 20:17:06 +0900

    문제와 풀이1

---
+++ Introduction_Java/Java-start/src/loop/ex/ForEx2.java
@@ -0,0 +1,11 @@
+package loop.ex;
+
+public class ForEx2 {
+    public static void main(String[] args) {
+
+        int count = 2;
+        for(int i=1; i<=10; i++){
+            System.out.println(i*count);
+        }
+    }
+}
