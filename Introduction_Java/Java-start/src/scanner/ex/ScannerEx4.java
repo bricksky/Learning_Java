e96f57c50c14e18e59f8e632d6113bc3065f4a55
bricksky <skydh1214@naver.com>
2024-07-13 22:48:03 +0900

    문제와 풀이2

--- Introduction_Java/Java-start/src/scanner/ex/ScannerEx4.java
+++ Introduction_Java/Java-start/src/scanner/ex/ScannerEx4.java
@@ -9,7 +9,7 @@ public class ScannerEx4 {
         System.out.print("구구단의 단수를 입력하세요: ");
         int n = scanner.nextInt();
 
-        System.out.println(n + "단의 구구단: ");
+        System.out.println(n + "단ㅌㅋ의 구구단: ");
         for(int i = 1; i<=9; i++){
             System.out.println(n + "x" + i + " = " + n * i);
 
