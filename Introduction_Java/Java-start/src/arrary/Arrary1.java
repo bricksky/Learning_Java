acce3f299a4e2d81939bc41ff431592d92783c23
bricksky <skydh1214@naver.com>
2024-07-14 13:26:04 +0900

    배열 시작

---
+++ Introduction_Java/Java-start/src/arrary/Arrary1.java
@@ -0,0 +1,17 @@
+package arrary;
+
+public class Arrary1 {
+    public static void main(String[] args) {
+        int student1 = 90;
+        int student2 = 80;
+        int student3 = 70;
+        int student4 = 90;
+        int student5 = 100;
+
+        System.out.println("학생1의 점수: " + student1);
+        System.out.println("학생2의 점수: " + student2);
+        System.out.println("학생3의 점수: " + student3);
+        System.out.println("학생4의 점수: " + student4);
+        System.out.println("학생5의 점수: " + student5);
+    }
+}
