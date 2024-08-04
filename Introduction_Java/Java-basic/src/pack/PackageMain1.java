8c299ebf35db7b9550e27064d29bfe542b6e24ff
bricksky <skydh1214@naver.com>
2024-08-04 22:54:24 +0900

    문제와 풀이

---
+++ Introduction_Java/Java-basic/src/pack/PackageMain1.java
@@ -0,0 +1,9 @@
+package pack;
+
+public class PackageMain1 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        // 다른 위치에 있는 경우 full 명칭을 적어줘야 함.
+        pack.a.User user = new pack.a.User();
+    }
+}
