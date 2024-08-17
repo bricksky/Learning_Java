d54f2ac196462b213b141081d8088ee90cb583e7
bricksky <skydh1214@naver.com>
2024-08-17 14:03:55 +0900

    문제와 풀이

--- Introduction_Java/Java-basic/src/static2/ex/MathArraryUtilsMain.java
+++ Introduction_Java/Java-basic/src/static2/ex/MathArraryUtilsMain.java
@@ -1,12 +1,14 @@
 package static2.ex;
 
+import static static2.ex.MathArrartUtil.*;
+
 public class MathArraryUtilsMain {
 
     public static void main(String[] args) {
         int[] values = {1, 2, 3, 4, 5};
-        System.out.println("sum = " + MathArrartUtil.sum(values));
-        System.out.println("average = " + MathArrartUtil.average(values));
-        System.out.println("min = " + MathArrartUtil.min(values));
-        System.out.println("max = " + MathArrartUtil.max(values));
+        System.out.println("sum = " + sum(values));
+        System.out.println("average = " + average(values));
+        System.out.println("min = " + min(values));
+        System.out.println("max = " + max(values));
     }
 }
