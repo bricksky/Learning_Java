ed5afceeed13e59c83cc140936404aec9d39a8dc
bricksky <skydh1214@naver.com>
2024-08-22 22:51:11 +0900

    문제와 풀이

---
+++ Introduction_Java/Java-basic/src/extends1/ex/Album.java
@@ -0,0 +1,11 @@
+package extends1.ex;
+
+public class Album extends Item{
+    public String artits;
+
+    // command + n -> 생성자 자동으로 만들어주는 단축키.
+    public Album(String name, int price, String artits) {
+        super(name, price);
+        this.artits = artits;
+    }
+}
---
+++ Introduction_Java/Java-basic/src/extends1/ex/Movie.java
@@ -0,0 +1,11 @@
+package extends1.ex;
+
+public class Movie extends Item{
+    private String director;
+    private String actor;
+
+    public Movie(String name, int price, String director, String actor) {
+        super(name, price);
+        this.actor = actor;
+    }
+}
