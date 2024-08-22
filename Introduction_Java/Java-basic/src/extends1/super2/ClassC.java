288c1e8b5762dbb56c5bb8878b34539e07f79d67
bricksky <skydh1214@naver.com>
2024-08-22 22:29:11 +0900

    super - 생성자

--- Introduction_Java/Java-basic/src/extends1/super2/ClassC.java
+++ Introduction_Java/Java-basic/src/extends1/super2/ClassC.java
@@ -4,8 +4,10 @@ public class ClassC extends ClassB {
 
     public ClassC(){
 //        super(); 기본 생성자가 아니기에 직접 설정해주어야 함.
-        super(10, 20);
+//        super(10, 20);
+        super(30);
         System.out.println("ClassC 생성자");
+
     }
 
 }
