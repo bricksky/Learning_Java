af2acc9035f104e975de179258285b317fbfe2c6
bricksky <skydh1214@naver.com>
2024-08-23 22:53:53 +0900

    다형성 시작

---
+++ Introduction_Java/Java-basic/src/poly/basic/Child.java
@@ -0,0 +1,9 @@
+package poly.basic;
+
+import extends1.access.parent.Parent;
+
+public class Child extends Parent {
+    public void childMethod(){
+        System.out.println("Child.childMethod");
+    }
+}
