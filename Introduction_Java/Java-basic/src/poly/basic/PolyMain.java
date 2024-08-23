df656f7e484c4b78884d1771375b42cbb63f8764
bricksky <skydh1214@naver.com>
2024-08-23 23:05:25 +0900

    다형성 시작

--- Introduction_Java/Java-basic/src/poly/basic/PolyMain.java
+++ Introduction_Java/Java-basic/src/poly/basic/PolyMain.java
@@ -12,5 +12,13 @@ public class PolyMain {
         Child child = new Child();
         child.parentMethod();
         child.childMethod();
+
+        // 부모 변수가 자식 인스턴스 참조
+        System.out.println("Parent -> Child");
+        Parent poly = new Child();
+        poly.parentMethod();
+
+        // 자식 변수가 부모 인스턴스 참조 (자식은 부모를 담을 수 없다.)
+        // Child child1 = new Parent();
     }
 }
