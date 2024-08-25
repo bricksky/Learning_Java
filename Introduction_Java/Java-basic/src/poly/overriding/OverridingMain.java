63713d729c17555c048b75de665c692fdcde0d06
bricksky <skydh1214@naver.com>
2024-08-25 14:11:27 +0900

    다형성과 메서드 오버라이딩

--- Introduction_Java/Java-basic/src/poly/overriding/OverridingMain.java
+++ Introduction_Java/Java-basic/src/poly/overriding/OverridingMain.java
@@ -18,6 +18,6 @@ public class OverridingMain {
         Parent poly = new Child();
         System.out.println("Parent -> Child");
         System.out.println("value = " + poly.value);    // 변수는 오버라이딩 X
-        poly.method();  // 메서드는 오버라이딩.
+        poly.method();  // 메서드는 오버라이딩가능
     }
 }
