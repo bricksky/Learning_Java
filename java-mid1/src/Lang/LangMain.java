d613885f12dda4b546920e39776ff328c05cfcb7
bricksky <skydh1214@naver.com>
2024-10-07 23:04:28 +0900

    java.lang 패키지 소개

---
+++ java-mid1/src/Lang/LangMain.java
@@ -0,0 +1,10 @@
+package Lang;
+
+// 반복적으로 사용하기에 생략 가능!
+// import java.lang.System;
+
+public class LangMain {
+    public static void main(String[] args) {
+       System.out.println("hello java");
+    }
+}
