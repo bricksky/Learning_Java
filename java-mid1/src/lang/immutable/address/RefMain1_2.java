e6f44f1397bed4d961002782a87aba131412551a
bricksky <skydh1214@naver.com>
2024-10-13 23:21:22 +0900

    공유 참조와 사이드 이펙트

--- java-mid1/src/lang/immutable/address/RefMain1_2.java
+++ java-mid1/src/lang/immutable/address/RefMain1_2.java
@@ -1,19 +1,24 @@
 package lang.immutable.address;
 
-public class RefMain_1 {
+public class RefMain1_2 {
 
     public static void main(String[] args) {
         // 참조형 변수는 하나의 인스턴스를 공유할 수 있음.
 
-    Address a = new Address("서울");
+    Address a = new Address("서울");  // xoo1
     Address b = a;
 
     System.out.println("a = " + a);
     System.out.println("b = " + b);
 
-    b.setValue("부산");
-    System.out.println("부산 -> b");
+    change(b, "부산");
     System.out.println("a = " + a);
     System.out.println("b = " + b);
     }
+
+    private static void change(Address address, String changeAddress){
+        System.out.println("주소값을 변경합니다. -> " + changeAddress);
+        address.setValue(changeAddress);
+    }
+
 }
