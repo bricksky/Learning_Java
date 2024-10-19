8b87ee96acd024454c7feb1f816dca94c59fb72d
bricksky <skydh1214@naver.com>
2024-10-19 23:52:02 +0900

    String 클래스 - 주요 메서드1

---
+++ java-mid1/src/lang/string/method/StringSearchMain.java
@@ -0,0 +1,13 @@
+package lang.string.method;
+
+public class StringSearchMain {
+    public static void main(String[] args) {
+
+        String str1 = "Hello, Java! Wlecome to Java world";
+
+        System.out.println("문자열에 Java가 포함되어 있는지: " + str1.contains("Java"));
+        System.out.println("Java의 첫 번째 인덱싀: " + str1.indexOf("Java"));
+        System.out.println("인덱스 10부터 'Java'의 인덱스: " + str1.indexOf("Java", 10));
+        System.out.println("Java의 마지막 인덱스: " + str1.lastIndexOf("Java"));
+    }
+}
