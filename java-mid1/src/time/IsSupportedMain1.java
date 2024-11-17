430c4919f5036e7ec1a5f1feecf46ac9b83f73d0
bricksky <skydh1214@naver.com>
2024-11-17 23:24:46 +0900

    날짜와 시간 조회하고 조작하기1

---
+++ java-mid1/src/time/IsSupportedMain1.java
@@ -0,0 +1,17 @@
+package time;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+public class IsSupportedMain1 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        int minute = now.get(ChronoField.SECOND_OF_MINUTE);
+        System.out.println("minute = " + minute);
+    }
+}
+
+/*
+Unsupported field: SecondOfMinute
+위와 같은 오류 메시지 발생한다.
+ */
