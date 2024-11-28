5d3eb069f912aa99fdf7cbc101f2aec7562e9e5e
bricksky <skydh1214@naver.com>
2024-11-28 19:36:23 +0900

    예외 처리가 필요한 이유3 - 반환 값으로 예외처리

--- java-mid1/src/exception/ex1/NetworkServiceV1_2.java
+++ java-mid1/src/exception/ex1/NetworkServiceV1_2.java
@@ -7,18 +7,23 @@ public class NetworkServiceV1_2 {
         NetworkClientV1 client = new NetworkClientV1(address);
         client.initError(data); // 추가
 
+        // 결과가 성공이 아니다? -> 오류다.
         String connectResult = client.connect();
-        if(!connectResult.equals("success")) {
+        if(isError(connectResult)) {
             System.out.println("[네트워크 오류 발생] 오류 코드: " + connectResult);
             return;
         }
-        
+
         String sendResult = client.send(data);
-        if(!sendResult.equals("success")) {
+        if(isError(sendResult)) {
             System.out.println("[네트워크 오류 발생] + 오류 코드: " + sendResult);
             return;
         }
 
         client.disconnect();
+    }
+
+    private static boolean isError(String connectResult) {
+        return !connectResult.equals("success");
     }
 }
