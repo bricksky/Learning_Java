35e0bd980af89dca9b2e0f40678c32365550ca5a
bricksky <skydh1214@naver.com>
2024-11-30 20:48:04 +0900

    예외 처리 도입5 - finally

--- java-mid1/src/exception/ex2/MainV2.java
+++ java-mid1/src/exception/ex2/MainV2.java
@@ -6,7 +6,7 @@ public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2 {
 
 //        NetworkServiceV2_1 service = new NetworkServiceV2_1();
-        NetworkServiceV2_4 service = new NetworkServiceV2_4();
+        NetworkServiceV2_5 service = new NetworkServiceV2_5();
         Scanner scanner = new Scanner(System.in);
 
         while(true){
