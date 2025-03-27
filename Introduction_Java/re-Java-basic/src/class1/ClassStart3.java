523c1c0c879f8fe2ac2bb02b905e2053b50e9c4d
bricksky <skydh1214@naver.com>
2025-03-27 17:31:08 +0900

    객체 사용

--- Introduction_Java/re-Java-basic/src/class1/ClassStart3.java
+++ Introduction_Java/re-Java-basic/src/class1/ClassStart3.java
@@ -5,6 +5,8 @@ public class ClassStart3 {
 
         // 클래스에 의해 만들어진 student1은 "객체 또는 인스턴스"
         Student student1 = new Student();
+
+        // .은 메모리에 존재하는 객체에 접근하는 키워드이다.
         student1.name = "학생1";
         student1.age = 15;
         student1.grade = 90;
@@ -13,6 +15,9 @@ public class ClassStart3 {
         student2.name = "학생2";
         student2.age = 18;
         student2.grade = 80;
+
+        System.out.println(student1);
+        System.out.println(student2);
 
         System.out.println("이름: " + student1.name + ", 나이: " + student1.age + ", 성적: " + student1.grade);
         System.out.println("이름: " + student2.name + ", 나이: " + student2.age + ", 성적: " + student2.grade);
